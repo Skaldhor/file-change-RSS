ed664e4520fa39aa047c3a24f7ffbab05e15cb87
Skaldhor <31501365+Skaldhor@users.noreply.github.com>
2021-06-20 15:44:56 +0200

    Add files via upload

--- FCR_setup.sh
+++ FCR_setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#set installation folder for file-change-rss
+# set installation folder for file-change-rss
 FILECHANGERSS="/home/pi/file-change-rss"
 
 
 ##### script - do not change #####
 
-#create install-, log- and tmp-folders, if they aren't present already
+# create install-, log- and tmp-folders, if they aren't present already
 [ ! -d "$FILECHANGERSS" ] && mkdir $FILECHANGERSS
 [ ! -d "$FILECHANGERSS/log" ] && mkdir $FILECHANGERSS/log
 [ ! -d "$FILECHANGERSS/tmp" ] && mkdir $FILECHANGERSS/tmp
 
-#download additional files
+# download additional files
 curl -L https://github.com/Skaldhor/file-change-RSS/releases/download/v0.1-alpha/file-change-RSS.ps1 -o $FILECHANGERSS/file-change-RSS.ps1
 curl -L https://github.com/Skaldhor/file-change-RSS/releases/download/v0.1-alpha/inotify.sh -o $FILECHANGERSS/inotify.sh
 curl -L https://github.com/Skaldhor/file-change-RSS/releases/download/v0.1-alpha/FCR.config -o $FILECHANGERSS/FCR.config
 
-#make inotify executable
+# make inotify executable
 chmod +x $FILECHANGERSS/inotify.sh
 
-#create cron jobs
+# create cron jobs
 crontab -l > $FILECHANGERSS/cronjobs
 echo '@reboot sleep 60 && screen -dmS filechangerss && screen -S filechangerss -X stuff "'$FILECHANGERSS/inotify.sh'^M"' >> $FILECHANGERSS/cronjobs
 echo "*/5 * * * * pwsh $FILECHANGERSS/file-change-RSS.ps1" >> $FILECHANGERSS/cronjobs
--- file-change-RSS.ps1
+++ file-change-RSS.ps1
@@ -1,7 +1,7 @@
-#importing variables
+# importing variables
 Get-Content $PSScriptRoot/FCR.config | Where-Object {$_.StartsWith("$")} | ForEach-Object {Invoke-Expression "$_" }
 
-#creating functions
+# creating functions
 function RSS_items($date, $path, $event){
 Add-Content $tmp_RSS_items "        <item>
             <title>Event $event for file: $path</title>
@@ -13,17 +13,17 @@ Add-Content $tmp_RSS_items "        <item>
         </item>"
 }
 
-#delete existing (if present) and create new empty tmp files
+# delete existing (if present) and create new empty tmp files
 If (Test-Path $tmp_RSS_items) { Remove-Item $tmp_RSS_items -Force }
 New-Item -Path $tmp_RSS_items -ItemType "file" -Force >$null 2>&1
 If (Test-Path $tmp_inverted_inotify_log) { Remove-Item $tmp_inverted_inotify_log -Force }
 New-Item -Path $tmp_inverted_inotify_log -ItemType "file" -Force >$null 2>&1
 
-#inverting inotify log
+# inverting inotify log, so the latest entries are on top
 $log = Get-Content -Path $inotify_log
 Add-Content -Path $tmp_inverted_inotify_log -Value ($log[($log.Length-1)..0])
 
-#writing RSS items to tmp_RSS_items file
+# writing RSS items to tmp_RSS_items file
 $input = Get-Content $tmp_inverted_inotify_log
 
 foreach ($line in $input)
@@ -35,26 +35,26 @@ foreach ($line in $input)
     RSS_items $date $path $event
 }
 
-#delete existing (if present) and create new final RSS file with header
+# delete existing (if present) and create new final RSS file with header
 If (Test-Path $RSS_output_file) { Remove-Item $RSS_output_file -Force }
 New-Item -Path $RSS_output_file -ItemType "file" -Force >$null 2>&1
 
 
 Add-Content $RSS_output_file "<?xml version=""1.0"" encoding=""utf-8""?>
-<rss version=""2.0"">
+<rss version=""2.0"" xmlns:media=""http://search.yahoo.com/mrss/"">
 	<channel>
 		<title>$channel_name</title>
 		<link></link>
 		<description>$channel_description</description>"
 		
-#add items to final RSS file
+# add items to final RSS file
 $items = Get-Content $tmp_RSS_items
 Add-Content $RSS_output_file $items
 
-#close remaining RSS tags
+# close remaining RSS tags
 Add-Content $RSS_output_file "	</channel>
 </rss>"
 
-#delete tmp files
+# delete tmp files
 Remove-Item $tmp_RSS_items -Force
 Remove-Item $tmp_inverted_inotify_log -Force
