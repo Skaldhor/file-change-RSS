4198b2a86831b0965bd4c9921cb4d0df455978c3
Skaldhor <31501365+Skaldhor@users.noreply.github.com>
2021-06-20 22:51:01 +0200

    Delete inotify.sh

--- inotify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# read variables
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source $SCRIPT_DIR/FCR.config 2>/dev/null
-
-# start inotify
-inotifywait -mr -e create -e delete -e modify -e move --format "%T;%w%f;%e" --timefmt "%a, %d. %b. %G, %T" $FOLDERTOMONITOR >> $INOTIFY_LOG
